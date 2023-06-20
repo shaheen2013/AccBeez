e7122f32d3289b6366b8e473fef5b459e7aedbec
shaheen2013 <coredeveloper.2013@gmail.com>
2023-06-20 22:02:13 +0600

    User join with adminUsers

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function adminUsers() {
+        return $this->hasMany(UserAssign::class, 'admin_id', 'id')->whereNotNull('user_id');
+    }
 }
