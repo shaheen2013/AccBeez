ca48e937ce1addd18b5ad3ea2ca79ee10ffda914
shaheen2013 <coredeveloper.2013@gmail.com>
2023-07-05 18:04:13 +0600

    Slug in company table

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -15,19 +15,24 @@ class CompanySeeder extends Seeder
     {
         DB::table('companies')->insert([
             [
-                'name'=>'Company 01'
+                'name'=>'Company 01',
+                'slug' => 'company-01'
             ],
             [
-                'name'=>'Company 02'
+                'name'=>'Company 02',
+                'slug' => 'company-02'
             ],
             [
-                'name'=>'Company 03'
+                'name'=>'Company 03',
+                'slug' => 'company-03'
             ],
             [
-                'name'=>'Company 04'
+                'name'=>'Company 04',
+                'slug' => 'company-04'
             ],
             [
-                'name'=>'Company 05'
+                'name'=>'Company 05',
+                'slug' => 'company-05'
             ],
         ]);
     }
